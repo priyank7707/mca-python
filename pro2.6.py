7a23ba40024c5daeadce99f432bc329e3ffa1707
priyank7707 <arvindtanna1975@gmail.com>
2026-03-06 11:16:08 +0530

    Add files via upload

---
+++ pro2.6.py
@@ -0,0 +1,8 @@
+# 6) Write a program to read a file which contains only numbers. Display total
+# of all numbers with maximum and minimum number.
+
+file = open("number.txt","r")
+content = file.read()
+
+
+print("content pf file : ",content)
